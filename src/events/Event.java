77db3a205ea61e15ec1e2379462eeb434eb6c915
August Meyers <augustmeyers14@gmail.com>
2021-11-16 01:08:36 -0500

    Documented ScheduledEvent object

--- src/events/Event.java
+++ src/events/Event.java
@@ -2,7 +2,7 @@ package events;
 
 public interface Event {
 	public String getName();
-
+	
 	public String getDescription();
 
 	public long getTime();
--- src/events/ScheduledEvent.java
+++ src/events/ScheduledEvent.java
@@ -2,10 +2,16 @@ package events;
 
 public class ScheduledEvent implements Event, Comparable<Event> {
 	
+	/*
+	 * Implements Event interface that provides the mandatory Methods
+	 * Implements Comparable to ensure that it is able to be compared 
+	 */
+	
 	private String eventName;
 	private String eventDescription;
 	private long eventTime;
-	
+	// Private instance variables 
+
 	private long eventOffHold;
 	// UNIX Time when the event is scheduled for 
 	
@@ -16,7 +22,8 @@ public class ScheduledEvent implements Event, Comparable<Event> {
 		eventOffHold = -1;
 		
 	}
-	
+	// Default constructor that just sets values generically 
+
 	public ScheduledEvent(String eventName, String eventDescription, long eventTime, long eventOffHold) {
 		this.eventName = eventName;
 		this.eventDescription = eventDescription;
@@ -24,31 +31,38 @@ public class ScheduledEvent implements Event, Comparable<Event> {
 		this.eventOffHold = eventOffHold;
 		
 	}	
-	
+	// Overloaded constructor for initializing the event
+
 	public String getName() {
 		return eventName;
 	
 	}
-
+	// Get method for returning the name of the event 
+	
 	public String getDescription() {
 		return eventDescription;
 	
 	}
-
+	// Get method for returning the description of the event 
+	
 	public long getTime() {
 		return eventTime;
 	
 	}
-
+	// Get method for returning the time the event was created
+	
 	public long getOffHold() {
 		return eventOffHold;
 		
 	}
+	// Get method for returning the time the event is scheduled for
 	
 	public String toString() {
 		return "ScheduledEvent," + eventName + "," + eventDescription + "," + eventTime + "," + eventOffHold;
 		
 	}
+	// toString method to return a formatted String of this Object
+	// Particularly, we use this for storing the Object
 
 	@Override
 	public int compareTo(Event o) {
