d07d90f7f118f4d51867547d049b77b3633f2ff7
August Meyers <augustmeyers14@gmail.com>
2021-12-06 17:29:42 -0500

    Added logging class

--- src/main/Log.java
+++ src/main/Log.java
@@ -1,3 +1,10 @@
+/*
+ * Procrastination Application
+ * August Meyers
+ * Log.java
+ * Logging Class
+ */
+
 package main;
 
 import java.io.BufferedWriter;
@@ -6,9 +13,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 
+	/**
+	 * Log Class
+	 * Wraps Logger
+	 */
+
 public class Log {
 	private static File logLocation = new File(System.getenv("APPDATA") + "/ProcrastinationApplication/Log.txt");
-
+	// Static file location 
+	
+	/**
+	 * writeLog Method
+	 * Takes static reference and writes the String to the log file 
+	 * 
+	 * @param String log
+	 */
+	
 	public static void writeLog(String log) {
 		checkFile();
 		
@@ -16,26 +36,38 @@ public class Log {
 		
 		try {
 			File = new BufferedWriter(new FileWriter(logLocation, true));
+			// Loads to file 
 			
 		} catch (IOException e) {
-			
+			// No point logging... There's no where for it to go!
+
 		}
 		
 		try {
 			File.append(System.currentTimeMillis() + " " + log + "\n");
 			File.flush();
+			// Writes to file 
 			
 		} catch (IOException e) {
+			// No point logging... There's no where for it to go!
 
 		}
 	}
+	
+	/**
+	 * checkFile Method
+	 * In case the log file has not already been created, it creates the log file
+	 * 
+	 */
 	
 	private static void checkFile() {
 		if (!logLocation.exists()) {
 			try {
 				logLocation.createNewFile();
+				// Creates file
 				
 			} catch (IOException e) {
+				// No point logging... There's no where for it to go!
 				
 			}
 		}
