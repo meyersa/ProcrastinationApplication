0a28b95a0204fd6d7bcc1e2214daccec17e9f4fe
August Meyers <augustmeyers14@gmail.com>
2021-12-06 14:39:25 -0500

    Added JavaDoc support

--- src/gui/MainGUI.java
+++ src/gui/MainGUI.java
@@ -1,6 +1,12 @@
+/*
+ * Procrastination Application
+ * August Meyers
+ * MainGUI.java
+ * Creates the MainGUI
+ */
+
 package gui;
 
-import javafx.event.ActionEvent;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import java.util.ArrayList;
@@ -29,30 +35,40 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+// All of the needed libraries for the GUI 
+
+	/*
+	 * MainGUI Class
+	 * The Main Class that outlines the GUI
+	 */
 
 public class MainGUI extends Application {
 	static private Map<Long, Event> allEvents;
-	static private ScrollPane eventsDisplay;
+	// For storing our Map between methods
 
+	static private ScrollPane eventsDisplay;
 	static private TextField creationName;
 	static private TextField creationTime;
 	static private TextField creationDescription;
 	static private AnchorPane errorPane;
 	static private Text errorField;
-
 	static private AnchorPane wrapEvents;
 	static private SplitPane SplitPane;
 	static private AnchorPane CreateBox;
+	// All of our datafields for pulling/setting information
+
+	/*
+	 * Start Method 
+	 * The class that actually wraps the GUI Required class for JavaFX
+	 * 
+	 * @param Stage primaryStage, input by JavaFX
+	 * @return void
+	 * @throws Exception
+	 */
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		System.out.println("MainGUI.start Method Called");
-
-		/*
-		 * MenuBar Creation Save - Calls the local save class with the current allEvents
-		 * Map Preferences - Calls the preferences menu, perhaps remove? About - Pulls
-		 * up the GitHub page? Add class for this
-		 */
 
 		// Create Menu --------------------------------------------------
 		MenuItem Save = new MenuItem("Save");
@@ -70,7 +86,6 @@ public class MainGUI extends Application {
 		Menu Menu = new Menu("File");
 		Menu.setId("Menu");
 		Menu.getItems().addAll(Save, About, Quit);
-		// TODO Fix stupid formatting
 
 		MenuBar MenuBar = new MenuBar(Menu);
 		MenuBar.setStyle("-fx-background-color: #e945ff");
@@ -152,8 +167,10 @@ public class MainGUI extends Application {
 		AnchorPane.setLeftAnchor(errorField, 0.0);
 		AnchorPane.setRightAnchor(errorField, 0.0);
 		AnchorPane.setBottomAnchor(errorField, 0.0);
+		// Centering it and setting it up for resizing
 
 		creationBlankField.setPadding(new Insets(0));
+		// Clears padding
 
 		VBox createFields = new VBox(creationBlankField, creationText, creationName, creationTime, creationDescription,
 				buttonRow, errorPane);
@@ -172,8 +189,6 @@ public class MainGUI extends Application {
 		 * Main Program Formatting Creates SplitPane to format, sets background color
 		 * Creates the Main VBox that formats the SplitPane and MenuBar Assigns to
 		 * Scene, assigns StyleSheet to the Scene Assigns to stage and shows
-		 * 
-		 * TODO Add a scene handler so multiple UIs can be shown
 		 */
 
 		// Main Formatting ----------------------------------------------
@@ -197,6 +212,12 @@ public class MainGUI extends Application {
 
 	}
 
+	/*
+	 * Clear Method 
+	 * Clears out all fields on GUI
+	 * 
+	 * @return void
+	 */
 	private void Clear() {
 		System.out.println("MainGUI.Create Method Called");
 
@@ -206,61 +227,100 @@ public class MainGUI extends Application {
 
 	}
 
+	/*
+	 * Create Method
+	 * Creates an Event based off of the filled fields, or doesn't if
+	 * they aren't filled
+	 * 
+	 * @return int Success value
+	 */
+
 	private int Create() {
 		System.out.println("MainGUI.Create Method Called");
 
 		/*
 		 * Takes input from creationName, creationTime, creationDescription Then calls
-		 * the showEvents class to display again If it fails, show errorPane
+		 * the showEvents method to display again If it fails, show errorPane
 		 */
 
 		if ((creationName.getText().equals("") && creationDescription.getText().equals(""))) {
 			errorPane.setVisible(true);
 			return 0;
-					
+			// If there is not enough information, errors and doesn't move forward
+
 		} else {
 			try {
 				if (creationTime.getText().equals("")) {
 					allEvents.put(System.currentTimeMillis(), new ReminderEvent(creationName.getText(),
 							creationDescription.getText(), System.currentTimeMillis()));
+					// If there is no schedule time, create it as a Reminder Event
 
 				} else {
 					allEvents.put(System.currentTimeMillis(),
 							new ScheduledEvent(creationName.getText(), creationDescription.getText(),
 									System.currentTimeMillis(), Long.parseLong(creationTime.getText())));
+					// If there is a creation time, create it as a Scheduled Event
 
 				}
 
 				errorPane.setVisible(false);
+				// Clears errors if successful
 
 			} catch (Exception e) {
 				errorPane.setVisible(true);
+				// If it fails to create an event, there is an error in the fields
 
 			}
 		}
 
 		showEvents();
+		// Calls showEvents to reformat data area
 
 		SplitPane.getItems().clear();
 		SplitPane.getItems().addAll(wrapEvents, CreateBox);
 		SplitPane.setDividerPositions(0.75);
+		// Refreshes SplitPane to reflect changes
 
 		return 1;
+		// Success
 
 	}
+
+	/*
+	 * menuQuit Method 
+	 * Quit button from Menu
+	 * 
+	 * @return void
+	 */
 
 	private void menuQuit() {
 		System.out.println("MainGUI.menuQuit Method Called");
 		end();
+		// Calls end method to quit
 
 	}
+
+	/*
+	 * About Method 
+	 * About button from Menu
+	 * 
+	 * @return void
+	 */
 
 	private void About() {
 		System.out.println("MainGUI.About Method Called");
 
 		getHostServices().showDocument("https://github.com/meyersa/ProcrastinationApplication");
+		// Opens default web browser to the GitHub page
 
 	}
+
+	/*
+	 * ShowEvents Method 
+	 * Creates all the events based off of the map
+	 * 
+	 * @return void
+	 */
 
 	private void showEvents() {
 		System.out.println("MainGUI.showEvents Method Called");
@@ -268,34 +328,49 @@ public class MainGUI extends Application {
 		int numOfEvents = -1;
 		List<AnchorPane> events = new ArrayList<AnchorPane>();
 		List<VBox> columns = new ArrayList<VBox>();
+		// Creates the list to store the variables in
 
-		allEvents = main.Sort.sortMapByValue(allEvents);
-		main.Notification.initNotification(allEvents);
+		try {
+			allEvents = main.Sort.sortMapByValue(allEvents);
+			main.Notification.initNotification(allEvents);
+			// Sorts the events upon change
+			// Reinitializes the notifications as well on change
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			// If allEvents is zero these may throw
+
+		}
 
 		try {
 			numOfEvents = allEvents.size();
-
+			// if allEvents is 0 this will throw 
+			
 		} catch (Exception e) {
 			System.out.println("Unable to get size of event map. " + e);
 			numOfEvents = 0;
-
+			// Defaults to 0 to process 
+			
 		}
 
 		System.out.println("-- Number of Events to Process " + numOfEvents + " --");
 
 		if (numOfEvents == 0 || numOfEvents < 0) {
 			events.add(vboxCreater());
-
+			// If there are no events, show default GUI 
+			
 		} else {
 			events = vboxCreatorInput();
-
+			// Else create the GUI boxes based off of the events 
+			
 		}
 
 		for (AnchorPane current : events) {
 			current.setStyle("-fx-background-color: #424144; -fx-border-color: #ccc2cc");
 			current.setMinSize(300, 250);
 			current.setMaxSize(300, 250);
-
+			// Styles all of the boxes in mass 
+			
 		}
 
 		if (events.size() == 1) {
@@ -312,6 +387,7 @@ public class MainGUI extends Application {
 
 			}
 		}
+		// Logic for deciding how many rows/columns to make to display the boxes
 
 		HBox HBox = new HBox();
 		HBox.getChildren().addAll(columns);
@@ -322,7 +398,9 @@ public class MainGUI extends Application {
 		HBox.setPadding(new Insets(25));
 		HBox.setMinWidth(1200);
 		HBox.setSpacing(25);
-
+		// By default we start with 1200 width because the background will show white when empty
+		// It will still autogrow 
+		
 		eventsDisplay = new ScrollPane(HBox);
 		eventsDisplay.setHbarPolicy(ScrollBarPolicy.ALWAYS);
 		eventsDisplay.setPannable(true);
@@ -338,7 +416,14 @@ public class MainGUI extends Application {
 		AnchorPane.setBottomAnchor(eventsDisplay, 0.0);
 
 	}
-
+	 
+	/*
+	 * vboxCreator Method
+	 * Creates default GUI box
+	 * 
+	 * @return AnchorPane default box
+	 */
+	
 	private AnchorPane vboxCreater() { // Default version
 		System.out.println("MainGUI.vboxCreator Method called");
 
@@ -364,6 +449,13 @@ public class MainGUI extends Application {
 
 	}
 
+	/*
+	 * vboxCreateInput Method
+	 * Creates a list of boxes with allEvents events display
+	 * 
+	 * @return List<AnchorPane> Events as boxes
+	 */
+	
 	private List<AnchorPane> vboxCreatorInput() {
 		System.out.println("MainGUI.vboxCreatorInput Method Called");
 
@@ -379,9 +471,12 @@ public class MainGUI extends Application {
 			currentEvent = allEvents.get(current);
 
 			if (currentEvent instanceof ReminderEvent) {
+				// If it is a reminderEvent, we make it this way 
+				
 				Name = new Text(currentEvent.getName());
 				Description = new Text(currentEvent.getDescription());
-
+				// Default values 
+				
 				Name.setFill(Color.web("#ebe1eb"));
 				Name.setStyle("-fx-font-size: 30");
 
@@ -411,6 +506,8 @@ public class MainGUI extends Application {
 				eventList.add(new AnchorPane(defaultEvent));
 
 			} else if (currentEvent instanceof ScheduledEvent) {
+				// If it is a ScheduledEvent, we make it this way 
+				
 				Name = new Text(currentEvent.getName());
 				Time = new Text((new java.util.Date(((ScheduledEvent) currentEvent).getOffHold()).toString()));
 				Description = new Text(currentEvent.getDescription());
@@ -461,17 +558,38 @@ public class MainGUI extends Application {
 
 	}
 
-	private void Delete(Event currentEvent) {		
+	/*
+	 * Delete Method
+	 * Deletes the current Event
+	 * 
+	 * @param Event currentEvent that should be deleted
+	 * @return void
+	 */
+	
+	private void Delete(Event currentEvent) {
+		System.out.println("MainGUI.Delete Method Called");
+		
 		allEvents.remove(currentEvent.getTime());
+		// Removes the event from the list 
 		
 		showEvents();
+		// Refreshes boxes
 
 		SplitPane.getItems().clear();
 		SplitPane.getItems().addAll(wrapEvents, CreateBox);
 		SplitPane.setDividerPositions(0.75);
+		// Refreshes display
 		
 	}
 
+	/*
+	 * Start Method
+	 * Starts the GUI utilizing the same thread
+	 * 
+	 * @param Map<Long, Event> allEvents list of Events
+	 * @return void 
+	 */
+	
 	public void start(Map<Long, Event> allEvents) {
 		System.out.println("MainGUI.start Method Called");
 
@@ -481,33 +599,67 @@ public class MainGUI extends Application {
 
 	}
 
+	/*
+	 * end Method
+	 * Quits GUI
+	 * 
+	 * @return void
+	 */
+	
 	public void end() {
 		System.out.println("MainGUI.end Method Called");
 
 		Save();
-
+		// Saves the current method
+		
 		try {
 			Platform.exit();
 			System.exit(0);
-
+			// Exits GUI and Java process
+			
 		} catch (Exception e) {
 			System.out.println(e.toString());
-			
+
 		}
 	}
 
+	/*
+	 * Save Method
+	 * Saves the current events
+	 * 
+	 * @return void
+	 */
+	
 	private void Save() {
 		System.out.println("MainGUI.Save Method Called");
 		main.LocalStorage.writeCache(allEvents);
-
+		// Writes events to cache
+		
 	}
 
+	/*
+	 * displayError Method
+	 * Displays errors to the errorField
+	 * 
+	 * @param String errorText what to display
+	 * @return void
+	 */
+	
 	public static void displayError(String errorText) {
+		System.out.println("MainGUI.displayError Method Called");
+		
 		errorField.setText(errorText);
 		errorPane.setVisible(true);
 
 	}
 
+	/*
+	 * hideError Method
+	 * Hides the error on screen
+	 * 
+	 * @return void
+	 */
+	
 	public static void hideError() {
 		errorPane.setVisible(false);
 		errorField.setText("Input Invalid. Please try again");
